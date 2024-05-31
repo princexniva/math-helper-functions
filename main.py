adfa4891ca21c33375ec6457bdcb99e38715316d
Devesh-45 <devesh.ashok02@gmail.com>
2024-05-31 20:23:16 +0530

    This function will check whether the given number is armstrong or not

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+from armstrong import armstrong
 from evens import evens
 
 num = int(input("Enter a number: "))
 even_nmbers = evens(num)
 for i in even_nmbers:
     print(i)
-    
+
+N=int(input("Enter the Three Digit number: "))
+C=armstrong(N)
+if C:
+    print(N,"It is an armstrong number")
+else:
+    print(N,"It is not an armstrong number")    
