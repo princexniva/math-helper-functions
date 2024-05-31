c53dd6b9c5d29dac660e8a7b7552133f22dea924
yaa-its-kii <kiyshor25@gmail.com>
2024-05-31 20:24:00 +0530

    squares of numbers

--- main.py
+++ main.py
@@ -4,4 +4,12 @@ num = int(input("Enter a number: "))
 even_nmbers = evens(num)
 for i in even_nmbers:
     print(i)
-    
+
+
+   
+from squares import squares
+
+a=int(input("Enter the number:"))
+r=squares(a)
+for i in range(a):
+    print(i**2)
