a9df31a1126b286eae906525730b6a7a9f970ce0
AKash-47 <z.achilles555@gmail.com>
2024-05-31 20:23:22 +0530

    Code added

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from evens import evens
+from factorial import factorial
 
 num = int(input("Enter a number: "))
 even_nmbers = evens(num)
 for i in even_nmbers:
     print(i)
-    
+
+
+a=int(input("Enter the number: "))
+print(factorial(a))
