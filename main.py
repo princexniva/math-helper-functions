b032f80bb1f8cc96123dacad0224cbb82a22eb4d
kaminixkams <finnakms2024@gmail.com>
2024-05-31 20:23:42 +0530

    added a palindrome code

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from evens import evens
+from palindrome import palindrome
 
 num = int(input("Enter a number: "))
 even_nmbers = evens(num)
 for i in even_nmbers:
     print(i)
+
+c=input()
+if palindrome(c):
+    print("Its a palindrome number")
+else:
+    print("Its not a palindrome number")
+
     
